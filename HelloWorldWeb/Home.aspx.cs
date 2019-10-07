8e540dc4221af75d109c8dcae47a22e407aecd38
anselumana <anselumana@gmail.com>
2019-10-07 21:37:02 +0200

    Set new page title on page load.

--- HelloWorldWeb/Home.aspx.cs
+++ HelloWorldWeb/Home.aspx.cs
@@ -11,7 +11,10 @@ namespace HelloWorldWeb
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (!IsPostBack)
+            {
+                this.Title = "Hoome sweet home...";
+            }
         }
     }
 }
